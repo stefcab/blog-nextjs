cadfa598456aff255f4fefee43e4f307f42d5a09
stefanos <stefcab@gmail.com>
2024-08-19 17:51:41 -0300

    change style to green

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@ import Image from "next/image";
 import styles from '@/app/ui/styles/home.module.css';
 
 export default function Home() {
+
   return (
+
     <main className="flex items-center justify-center min-h-screen bg-gray-50">
+      
       <div className="container mx-auto p-4 relative">
         <div className="bg-white border-2 border-emerald-500 rounded-lg p-6 grid grid-cols-1 md:grid-cols-2 gap-4">
           <div className="flex flex-col justify-center">
@@ -36,5 +39,6 @@ export default function Home() {
       </div>
       <div className={`hidden md:block absolute top-0 right-0 bottom-0 left-2/3 z-0 ${styles.bg_green}`}></div>
     </main>
+
   );
 }
